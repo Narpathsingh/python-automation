992c5a8bebfd5dc50bc5412d03b6f3a6158ea888
Narpath Singh <narpathsingh208@gmail.com>
2025-01-26 19:49:58 +0530

    linkedin folder added

---
+++ linkedin/main.py
@@ -0,0 +1,44 @@
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+import time
+chromeoptions = webdriver.ChromeOptions()
+chromeoptions.add_experimental_option('detach', True)
+
+driver = webdriver.Chrome(chromeoptions)
+
+
+driver.get('https://unstop.com/auth/login')
+time.sleep(10)
+
+# submit email
+emailvalue = driver.find_element(By.NAME, value='email')
+# insert value
+# dummy mail to login = ksswccldetxdegklhe@ytnhy.com
+emailvalue.send_keys('ksswccldetxdegklhe@ytnhy.com')
+# print(emailvalue)
+# password field
+passvalue = driver.find_element(By.NAME, value='password')
+passvalue.send_keys('Bangalore@098')
+# print(passvalue.text)
+# print(passvalue)
+# submit button
+
+button = driver.find_element(By.CSS_SELECTOR, value='.submit_btn')
+button.click()
+
+# goto job portal
+jobportal = driver.get('https://unstop.com/job-portal')
+time.sleep(10)
+driveclick = driver.find_element(By.CSS_SELECTOR, value='.ng-star-inserted div')
+driveclick.click()
+print(driveclick.text)
+print(driveclick)
+# before quit
+# waittime = time.sleep(10)
+# to accept cookie
+
+# cookie = driver.find_element(By.CSS_SELECTOR, value='.GTM_ACCEPT_COOKIE')
+# print(cookie)
+
+# driver.quit()
+
